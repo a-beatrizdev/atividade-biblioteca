4a6eb131103ce36a31f226780da87ebae6734e80
Alana Beatriz <alanabeatriztobias@gmail.com>
2025-10-03 13:23:46 -0300

    add etapa 3 - função listagem de livros

--- main.py
+++ main.py
@@ -15,30 +15,44 @@ CREATE TABLE IF NOT EXISTS livros (
 """)
 
 
-def cadastrar_livro(título, autor, ano):
+# def cadastrar_livro(título, autor, ano):
+#     try:
+#         conexao = sqlite3.connect("biblioteca.db")
+#         cursor = conexao.cursor()
+    
+#         cursor.execute("""
+#         INSERT INTO livros (título, autor, ano, disponível)
+#         VALUES (?, ?, ?, ?)                                         
+#         """,
+#         (título, autor, ano, "sim")
+#         )
+#         conexao.commit()
+#     except Exception as erro:
+#         print(f"Erro ao tentar cadastrar o livro {erro} ")
+#     finally:
+#         if conexao:
+#             conexao.close()
+
+# título = input("Digite o título do livro: ").lower()
+# autor = input("Digite o autor do livro: ").lower()
+# ano = input("Digite o ano do livro: ")
+
+# cadastrar_livro(título, ano, autor)
+
+def listar_livros():
     try:
         conexao = sqlite3.connect("biblioteca.db")
         cursor = conexao.cursor()
-    
-        cursor.execute("""
-        INSERT INTO livros (título, autor, ano, disponível)
-        VALUES (?, ?, ?, ?)                                         
-        """,
-        (título, autor, ano, "sim")
-        )
-        conexao.commit()
-
+        cursor.execute("SELECT * FROM livros")
+        for linha in cursor.fetchall():
+            print(f"id: {linha[0]} | título: {linha[1]} | Autor: {linha[2]} | ano: {linha[3]} | disponível: {linha[4]} ")
     except Exception as erro:
-        print(f"Erro ao tentar cadastrar o livro {erro} ")
+        print(f"Erro ao listar o livro {erro} ")
     finally:
         if conexao:
             conexao.close()
+listar_livros()
 
-título = input("Digite o título do livro: ").lower()
-autor = input("Digite o autor do livro: ").lower()
-ano = input("Digite o ano do livro: ")
-
-cadastrar_livro(título, ano, autor)
 
 
 
