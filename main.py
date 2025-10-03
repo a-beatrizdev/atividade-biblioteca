4c71e6fa886713b25680c8c12be140a8cb989672
Alana Beatriz <alanabeatriztobias@gmail.com>
2025-10-03 14:47:41 -0300

    add etapa 4 - atualização de disponibilidade

--- main.py
+++ main.py
@@ -26,8 +26,10 @@ CREATE TABLE IF NOT EXISTS livros (
 #         """,
 #         (título, autor, ano, "sim")
 #         )
+
 #         conexao.commit()
 #     except Exception as erro:
+
 #         print(f"Erro ao tentar cadastrar o livro {erro} ")
 #     finally:
 #         if conexao:
@@ -39,19 +41,62 @@ CREATE TABLE IF NOT EXISTS livros (
 
 # cadastrar_livro(título, ano, autor)
 
-def listar_livros():
-    try:
-        conexao = sqlite3.connect("biblioteca.db")
-        cursor = conexao.cursor()
-        cursor.execute("SELECT * FROM livros")
-        for linha in cursor.fetchall():
-            print(f"id: {linha[0]} | título: {linha[1]} | Autor: {linha[2]} | ano: {linha[3]} | disponível: {linha[4]} ")
-    except Exception as erro:
-        print(f"Erro ao listar o livro {erro} ")
-    finally:
-        if conexao:
-            conexao.close()
-listar_livros()
+# def listar_livros():
+#     try:
+#         conexao = sqlite3.connect("biblioteca.db")
+#         cursor = conexao.cursor()
+
+#         cursor.execute("SELECT * FROM livros")
+#         for linha in cursor.fetchall():
+#             print(f"id: {linha[0]} | título: {linha[1]} | Autor: {linha[2]} | ano: {linha[3]} | disponível: {linha[4]} ")
+
+#     except Exception as erro:
+#         print(f"Erro ao listar o livro {erro} ")
+#     finally:
+#         if conexao:
+#             conexao.close()
+# listar_livros()
+
+
+# def atualizar_disponibilidade():
+#     try:
+#         conexao = sqlite3.connect("biblioteca.db")
+#         cursor = conexao.cursor()
+        
+#         id_livro = int(input("Digite o id do livro que deseja atualizar a disponibilidade: ").strip())
+#         nova_disponibilidade = input("Digite a nova disponibilidade (sim/não): ").lower().strip()
+
+#         cursor.execute("""
+#         UPDATE livros SET disponível = ? WHERE id = ?
+#         """, (nova_disponibilidade, id_livro))
+
+#         conexao.commit()
+#         if cursor.rowcount > 0:
+#             print("Disponibilidade atualizada com sucesso!")
+#         else:
+#             print("Nenhum livro encontrado com o id fornecido.")
+#     except Exception as erro:
+#         print("Erro ao atualizar disponibilidade:", {erro})
+#     finally:
+#         if conexao:
+#             conexao.close()
+# atualizar_disponibilidade()
+
+
+                       
+                       
+                       
+
+
+
+
+
+
+
+
+
+
+
 
 
 
